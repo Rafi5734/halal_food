42db688b3932d329623c750b97790e16cc8f693f
Rafi5734 <kamrulhasun5734@gmail.com>
2024-03-05 23:48:10 +0600

    order checkout page added

--- src/app/orderCheckout/page.js
+++ src/app/orderCheckout/page.js
@@ -2,11 +2,7 @@ import Checkout from "@/components/checkout/Checkout";
 import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <Checkout />
-    </div>
-  );
+  return <div>{/* <Checkout /> */}</div>;
 };
 
 export default page;
