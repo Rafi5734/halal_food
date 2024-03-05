56407c84043ed780949cc8a73f672f35d8a83243
Rafi5734 <kamrulhasun5734@gmail.com>
2024-03-05 21:22:21 +0600

    img issue solve

--- src/components/admin/allUsers/userList/UserList.js
+++ src/components/admin/allUsers/userList/UserList.js
@@ -203,7 +203,7 @@ const UserList = () => {
                 scope="row"
                 className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Apple MacBook Pro 17"
+                Apple MacBook Pro 17
               </th>
               <td className="px-6 py-4">Silver</td>
               <td className="px-6 py-4">Laptop</td>
@@ -358,7 +358,7 @@ const UserList = () => {
                 scope="row"
                 className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Apple iMac 27"
+                Apple iMac 27
               </th>
               <td className="px-6 py-4">Silver</td>
               <td className="px-6 py-4">PC Desktop</td>
