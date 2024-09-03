6147240b5ec4049a6bdb254b5624ac4b9ae82c66
Rafi5734 <kamrulhasun5734@gmail.com>
2024-09-03 22:12:11 +0600

    testing responsive of categoriews

--- src/components/navbar/updatedNavbar/UpdatedNavbar.jsx
+++ src/components/navbar/updatedNavbar/UpdatedNavbar.jsx
@@ -199,47 +199,48 @@ const UpdatedNavbar = () => {
           </Badge>
           {width && width <= 870 ? (
             <>
-              <Button
+              {/* <Button
                 onClick={handleCategory}
                 isIconOnly
                 variant="flat"
                 className="ms-1"
               >
                 <ThreeDotIcon />
-              </Button>{" "}
-              {/* <div className="flex items-center gap-4 ms-4">
+              </Button>{" "} */}
+              <div className="flex items-center gap-4">
                 <Dropdown placement="bottom-end">
                   <DropdownTrigger>
-                    <Avatar
+                    <Button
+                      // onClick={handleCategory}
+                      isIconOnly
+                      variant="flat"
+                      className="ms-1"
+                    >
+                      <ThreeDotIcon />
+                    </Button>
+                    {/* <Avatar
                       isBordered
                       as="button"
-                      className="transition-transform"
-                      src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                    />
+                      className="transition-transform w-10"
+                      src="https://i.ibb.co/hcF4cLg/three-dots-vertical-svgrepo-com.png"
+                    /> */}
                   </DropdownTrigger>
                   <DropdownMenu aria-label="Profile Actions" variant="flat">
-                    <DropdownItem key="profile" className="h-14 gap-2">
-                      <p className="font-semibold">Signed in as</p>
-                      <p className="font-semibold">zoey@example.com</p>
-                    </DropdownItem>
-                    <DropdownItem key="settings">My Settings</DropdownItem>
-                    <DropdownItem key="team_settings">
-                      Team Settings
-                    </DropdownItem>
-                    <DropdownItem key="analytics">Analytics</DropdownItem>
-                    <DropdownItem key="system">System</DropdownItem>
-                    <DropdownItem key="configurations">
-                      Configurations
-                    </DropdownItem>
-                    <DropdownItem key="help_and_feedback">
-                      Help & Feedback
-                    </DropdownItem>
-                    <DropdownItem key="logout" color="danger">
-                      Log Out
-                    </DropdownItem>
+                    {getAllCategories?.map((category) => (
+                      <DropdownItem key={category?.category}>
+                        <Link
+                          href={{
+                            pathname: "/categoryProducts",
+                            query: { category: category?.category },
+                          }}
+                        >
+                          {category?.category}
+                        </Link>
+                      </DropdownItem>
+                    ))}
                   </DropdownMenu>
                 </Dropdown>
-              </div> */}
+              </div>
             </>
           ) : (
             <></>
