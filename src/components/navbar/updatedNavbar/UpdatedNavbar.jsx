2d40032524ee886247afcb7bd2c4826222085686
Rafi5734 <kamrulhasun5734@gmail.com>
2024-09-03 21:59:27 +0600

    testing responsive of categoriews

--- src/components/navbar/updatedNavbar/UpdatedNavbar.jsx
+++ src/components/navbar/updatedNavbar/UpdatedNavbar.jsx
@@ -8,6 +8,7 @@ import {
   DropdownItem,
   User,
   Badge,
+  Avatar,
 } from "@nextui-org/react";
 
 import { Button } from "@nextui-org/react";
@@ -198,12 +199,47 @@ const UpdatedNavbar = () => {
           </Badge>
           {width && width <= 870 ? (
             <>
-              {/* <Button onClick={handleCategory} isIconOnly variant="flat">
+              <Button
+                onClick={handleCategory}
+                isIconOnly
+                variant="flat"
+                className="ms-1"
+              >
                 <ThreeDotIcon />
-              </Button>{" "} */}
-              <button onClick={handleCategory}>
-                <ThreeDotIcon />
-              </button>{" "}
+              </Button>{" "}
+              {/* <div className="flex items-center gap-4 ms-4">
+                <Dropdown placement="bottom-end">
+                  <DropdownTrigger>
+                    <Avatar
+                      isBordered
+                      as="button"
+                      className="transition-transform"
+                      src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                    />
+                  </DropdownTrigger>
+                  <DropdownMenu aria-label="Profile Actions" variant="flat">
+                    <DropdownItem key="profile" className="h-14 gap-2">
+                      <p className="font-semibold">Signed in as</p>
+                      <p className="font-semibold">zoey@example.com</p>
+                    </DropdownItem>
+                    <DropdownItem key="settings">My Settings</DropdownItem>
+                    <DropdownItem key="team_settings">
+                      Team Settings
+                    </DropdownItem>
+                    <DropdownItem key="analytics">Analytics</DropdownItem>
+                    <DropdownItem key="system">System</DropdownItem>
+                    <DropdownItem key="configurations">
+                      Configurations
+                    </DropdownItem>
+                    <DropdownItem key="help_and_feedback">
+                      Help & Feedback
+                    </DropdownItem>
+                    <DropdownItem key="logout" color="danger">
+                      Log Out
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </div> */}
             </>
           ) : (
             <></>
