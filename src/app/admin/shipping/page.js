fee34d9143eb9665acc5b2879b9e1fa5fe1f92cf
Rafi5734 <kamrulhasun5734@gmail.com>
2025-02-12 17:01:16 +0600

    error solved

--- src/app/admin/shipping/page.js
+++ src/app/admin/shipping/page.js
@@ -78,109 +78,107 @@ export default async function ShippingOrders() {
               </thead>
               <tbody>
                 {shippingOrders
-                  ?.map((product) => (
-                    <>
-                      {product?.order?.map((order, index) => (
-                        <tr
-                          key={order?._id}
-                          className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-                        >
-                          <td className="p-4">
-                            <Image
-                              src={JSON.parse(order?.color)?.url}
-                              className="w-16 md:w-32 max-w-full max-h-full rounded"
-                              alt="Product Image"
-                              width={100}
-                              height={100}
-                            />
-                            <p className="mt-3 font-bold">{order?.name}</p>
-                            <p className="mt-3 font-bold">
-                              Color: {JSON.parse(order?.color)?.title}
+                  ?.map((product) =>
+                    product?.order?.map((order, index) => (
+                      <tr
+                        key={`${product._id}-${order._id}`} // Unique key using both product and order ID
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                      >
+                        <td className="p-4">
+                          <Image
+                            src={JSON.parse(order?.color)?.url}
+                            className="w-16 md:w-32 max-w-full max-h-full rounded"
+                            alt="Product Image"
+                            width={100}
+                            height={100}
+                          />
+                          <p className="mt-3 font-bold">{order?.name}</p>
+                          <p className="mt-3 font-bold">
+                            Color: {JSON.parse(order?.color)?.title}
+                          </p>
+                        </td>
+                        {/* Merge rows for the first order */}
+                        {index === 0 && (
+                          <td
+                            className="px-6 py-4 font-semibold text-gray-900 dark:text-white"
+                            rowSpan={product.order.length}
+                          >
+                            <p>{product?.fullName}</p>
+                          </td>
+                        )}
+                        <td className="font-bold">
+                          {JSON.parse(order?.color)?.title}
+                        </td>
+                        <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                          {JSON.parse(order?.productQuantity)}
+                        </td>
+                        <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                          {JSON.parse(order?.size)}
+                        </td>
+                        <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white text-nowrap">
+                          {order?.price} TK
+                        </td>
+                        <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                          {order?.discount}%
+                        </td>
+                        <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                          {Number(
+                            order?.price -
+                              (order?.price * order?.discount) / 100
+                          ) * Number(JSON.parse(order?.productQuantity))}{" "}
+                          TK
+                        </td>
+                        <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                          {new Date(product?.orderTime).toLocaleDateString(
+                            "en-GB",
+                            {
+                              day: "2-digit",
+                              month: "short",
+                              year: "numeric",
+                            }
+                          )}
+                        </td>
+                        {/* Merge rows for the first order */}
+                        {index === 0 && (
+                          <td
+                            className="px-6 py-4 font-semibold text-gray-900 dark:text-white"
+                            rowSpan={product.order.length}
+                          >
+                            <p>
+                              Address details:{" "}
+                              <span className="font-extralight">
+                                {product?.division}; {product?.district};{" "}
+                                {product?.zilla}; {product?.address}
+                              </span>
+                            </p>
+                            <p>
+                              Phone Number:{" "}
+                              <span className="font-extralight">
+                                {product?.phoneNumber}
+                              </span>
                             </p>
                           </td>
-                          {index === 0 && (
-                            <td
-                              className="px-6 py-4 font-semibold text-gray-900 dark:text-white"
-                              rowSpan={product.order.length}
+                        )}
+                        <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white text-nowrap">
+                          {product?.status}
+                        </td>
+                        {/* Merge rows for the first order */}
+                        {index === 0 && (
+                          <td
+                            className="px-6 py-4 font-semibold text-gray-900 dark:text-white"
+                            rowSpan={product.order.length}
+                          >
+                            <button
+                              // onClick={() => handleDeleteProduct(product?._id)}
+                              className="font-medium text-red-600 dark:text-red-500 hover:underline"
                             >
-                              <p>{product?.fullName}</p>
-                            </td>
-                          )}
-                          <td className="font-bold">
-                            {JSON.parse(order?.color)?.title}
+                              Remove
+                            </button>
                           </td>
-                          <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                            {JSON.parse(order?.productQuantity)}
-                          </td>
-                          <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                            {JSON.parse(order?.size)}
-                          </td>
-                          <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white text-nowrap">
-                            {order?.price} TK
-                          </td>
-                          <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                            {order?.discount}%
-                          </td>
-                          <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                            {Number(
-                              order?.price -
-                                (order?.price * order?.discount) / 100
-                            ) * Number(JSON.parse(order?.productQuantity))}{" "}
-                            TK
-                          </td>
-                          <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                            {new Date(product?.orderTime).toLocaleDateString(
-                              "en-GB",
-                              {
-                                day: "2-digit",
-                                month: "short",
-                                year: "numeric",
-                              }
-                            )}
-                          </td>
-
-                          {index === 0 && (
-                            <td
-                              className="px-6 py-4 font-semibold text-gray-900 dark:text-white"
-                              rowSpan={product.order.length}
-                            >
-                              <p>
-                                Addres details:{" "}
-                                <span className="font-extralight">
-                                  {product?.division}; {product?.district};{" "}
-                                  {product?.zilla}; {product?.address}
-                                </span>
-                              </p>
-                              <p>
-                                Phone Number:{" "}
-                                <span className="font-extralight">
-                                  {product?.phoneNumber}
-                                </span>
-                              </p>
-                            </td>
-                          )}
-                          <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white text-nowrap">
-                            {product?.status}
-                          </td>
-                          {index === 0 && (
-                            <td
-                              className="px-6 py-4 font-semibold text-gray-900 dark:text-white"
-                              rowSpan={product.order.length}
-                            >
-                              <button
-                                // onClick={() =>
-                                //   handleDeleteProduct(product?._id)
-                                // }
-                                className="font-medium text-red-600 dark:text-red-500 hover:underline"
-                              >
-                                Remove
-                              </button>
-                            </td>
-                          )}
-                        </tr>
-                      ))}
-                    </>
-                  ))
+                        )}
+                      </tr>
+                    ))
+                  )
                   ?.reverse()}
               </tbody>
             </table>
